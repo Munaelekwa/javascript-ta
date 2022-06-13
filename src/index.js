e6eadc356ac581ad6b277534f6e9bd64d0263549
Munachiso Elekwa <munaelekwa@gmail.com>
2022-06-13 15:38:10 +0100

    I have added a solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 // complete the function
 function prime (num) {
-  // code goes here
+  // check if number is equal to 1 or 2
+  if (num === 1) {
+    console.log("1 is not a prime number.")
+  }
+  else if (num === 2) {
+    console.log("2 is a prime number.")
+    return true
+  }
+  // check if number is greater than 2
+  else if (num > 2) {
+    // loop through 2 to number-1
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+        console.log(`${num} is not a prime number.`)
+        return false
+      }
+
+      console.log(`${num} is a prime number.`)
+      return true
+    }
+  }
+  
 }
 
 function solution (arg) {
